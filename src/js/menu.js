4e345c15897f0c0fb417e05335029e4b5d1c1cd8
Guillaume Masclet <guillaume@masclet.net>
2021-03-29 23:30:24 +0200

    Met en place le menu mobile

--- src/js/menu.js
+++ src/js/menu.js
@@ -2,6 +2,14 @@
   'use scrict';
 
   var root = $('html, body');
+  var isMenuOpen = false;
+  var toggleMenu = function() {
+    $('nav .fas').toggle();
+    $('nav .entry').animate({
+      left: isMenuOpen ? '-120%' : '-40px'
+    }, 500);
+    isMenuOpen = !isMenuOpen;
+  }
 
   $(document).on('click', 'a[href^="#"]', function(event) {
     event.preventDefault();
@@ -9,5 +17,11 @@
     root.animate({
       scrollTop: $($.attr(this, 'href')).offset().top
     }, 1000);
+
+    if (isMenuOpen) {
+      toggleMenu();
+    }
   });
+
+  $('nav .fas').click(toggleMenu)
 })(window.jQuery);
