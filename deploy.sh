65d5d264da65b88448b1cddaace76563f8492689
Guillaume Masclet <guillaume@masclet.net>
2021-04-12 23:00:21 +0200

    Add a deploy script

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+set -ex
+
+SSH_USER=guillaume
+SSH_HOST=masclet.net
+REMOTE_PATH=/home/guillaume/www/guillaume.masclet.net/
+
+rsync -av --delete \
+    --exclude=deploy.sh \
+    --exclude=README.md \
+    --exclude=.git \
+    --exclude=.env \
+    --exclude=*.dist \
+    --exclude=node_modules \
+    --exclude=src \
+    --exclude=.gitignore \
+    --exclude=gulpfile.js \
+    --exclude=package.json \
+    --exclude=package-lock.json \
+    --exclude=.bin \
+    ./ "$SSH_USER@$SSH_HOST:$REMOTE_PATH"
